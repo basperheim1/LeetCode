df1c1dbf0aa99e5dd8a18fed3c94f59a402a6869
Ben Asperheim <basperheim1@gmail.com>
2025-10-04 10:49:48 -0700

    11

---
+++ 11/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        left_pointer = 0
+        right_pointer = n - 1
+
+        max_area = 0
+
+        while left_pointer != right_pointer: 
+            possible_area = (right_pointer - left_pointer)*min(height[left_pointer], height[right_pointer])
+
+            max_area = max(max_area, possible_area)
+
+            if height[left_pointer] > height[right_pointer]:
+                right_pointer -= 1
+
+            else:
+                left_pointer += 1
+
+        return max_area
