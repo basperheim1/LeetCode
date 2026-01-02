63a90007c59d764a52d688e7fbcffa8593c64d5e
Ben Asperheim <basperheim1@gmail.com>
2026-01-01 22:12:05 -0800

    961

---
+++ 961/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+
+        seen_nums = set()
+        for num in nums: 
+            if num in seen_nums:
+                return num
+
+            seen_nums.add(num)
+        
